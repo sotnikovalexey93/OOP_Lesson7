aeb1ead62ef43171b7a4bd5d75f82fa5fb451a9e
Alexey <sotnikovalexey93@mail.ru>
2023-06-17 18:51:42 +0300

    OOP

---
+++ src/main/java/ru/geekbrains/lesson7/observer/JobApplicantType.java
@@ -0,0 +1,19 @@
+package ru.geekbrains.lesson7.observer;
+
+public class JobApplicantType {
+    private int id;
+    private String name;
+
+    // Конструкторы, геттеры и сеттеры
+
+    public class JobApplicant {
+    private int id;
+    private String name;
+    private int age;
+    private JobApplicantType type;
+
+    // Конструкторы, геттеры и сеттеры
+}
+
+}
+
---
+++ src/main/java/ru/geekbrains/lesson7/observer/JobVacancy.java
@@ -0,0 +1,10 @@
+package ru.geekbrains.lesson7.observer;
+
+public class JobVacancy {
+    private int id;
+    private String title;
+    private String description;
+    private Company Company;
+
+    // Конструкторы, геттеры и сеттеры
+}
